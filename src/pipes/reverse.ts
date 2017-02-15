5e8aa21ef493fc3ef0a7b00d012888885df4c5f4
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-02-14 19:45:33 -0500

    add pipes

--- src/pipes/reverse.ts
+++ src/pipes/reverse.ts
@@ -8,7 +8,7 @@ export class ReversePipe {
   
   transform(value:string) {
     let resultado = '';
-    for(let i = (value.length-1); i != 0; i--){
+    for(let i = (value.length-1); i != -1; i--){
       resultado+=value[i];
     }
     return resultado;
